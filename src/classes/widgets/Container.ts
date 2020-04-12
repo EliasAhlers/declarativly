75a6cc7df53ac8f4da62d5ba9542176bfbd8b28f
ThatsEli <public@thatseliyt.de>
2020-04-12 13:29:19 +0200

    Working on DivElement state management

--- src/classes/widgets/Container.ts
+++ src/classes/widgets/Container.ts
@@ -1,17 +1,27 @@
-import { Widget } from "../Widget";
+import { Widget, State } from "../Widget";
 
 export class Container extends Widget {
 
     protected element: HTMLDivElement;
 
-    constructor(public child: Widget) {
+    constructor(child: Widget) {
         super();
+        this.widgetState['child'] = child;
         this.element = document.createElement('div');
-        this.element.appendChild(this.child.render());
+        this.element.appendChild(child.render());
+    }
+
+    public stateUpdated(): void {
+        this.element.innerHTML = '';
+        this.element.appendChild(this.widgetState['child'].render());
     }
 
     public reRender(widget: Container): void {
-        this.child.reRender(widget.child);
+        if(widget.widgetState != this.widgetState) {
+            this.widgetState = widget.widgetState;
+            this.stateUpdated();
+        }
+        this.widgetState['child'].reRender(widget.widgetState['child']);
     }
 
 }
--- src/classes/widgets/DivElement.ts
+++ src/classes/widgets/DivElement.ts
@@ -4,17 +4,35 @@ export class DivElement extends Widget {
 
     protected element: HTMLDivElement;
 
-    constructor(public children: Array<Widget>) {
+    constructor(children: Array<Widget>) {
         super();
         this.element = document.createElement('div');
-        this.children.forEach((child: Widget) => {
+        this.widgetState['children'] = children;
+        this.widgetState['children'].forEach((child: Widget) => {
             this.element.appendChild(child.render());
-        })
+        });
+    }
+
+    public stateUpdated(): void {
+        // for (let i = 0; i < this.element.children.length; i++) {
+        //     const htmlChild = this.element.children[i];
+        //     htmlChild.remove();
+        // }
+        this.element.innerHTML = '';
+        this.widgetState['children'].forEach((child: Widget) => {
+            this.element.appendChild(child.render());
+        });
     }
 
     public reRender(widget: DivElement): void {
-        this.children.forEach((child: Widget, index: number) => {
-            child.reRender(widget.children[index]);
+        // console.log('++++++++++++++', widget.widgetState['children'], '-', this.widgetState['children']);
+        // if(widget.widgetState['children'].length != this.widgetState['children'].length) {
+        if(widget.widgetState != this.widgetState) {
+            this.widgetState = widget.widgetState;
+            this.stateUpdated();
+        }
+        this.widgetState['children'].forEach((child: Widget, index: number) => {
+            child.reRender(widget.widgetState['children'][index]);
         });
     }
 
